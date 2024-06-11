16b518d21df089bde92543465ca480c92d9da0c8
ashuydv02 <ay545153@gmail.com>
2024-06-11 18:31:58 +0530

    None type in python

---
+++ none.py
@@ -0,0 +1,16 @@
+def example(arg=None):
+    if arg is None:
+        arg = []
+    
+def example():
+    pass
+
+result = example()
+print(result)  
+
+variable = None
+
+if variable is None:
+    print("Variable is None")
+
+
