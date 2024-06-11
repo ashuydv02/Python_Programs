cf15f0e0f5c1d92cec7a12bc158fd34a3f49117c
ashuydv02 <ay545153@gmail.com>
2024-06-11 13:11:23 +0530

    Decorators

---
+++ decorator.py
@@ -0,0 +1,42 @@
+def smart_divide(func):
+    def inner(a, b):
+        print("I am going to divide", a, "and", b)
+        if b == 0:
+            print("Whoops! cannot divide")
+            return
+
+        return func(a, b)
+    return inner
+
+@smart_divide
+def divide(a, b):
+    print(a/b)
+
+divide(2,5)
+divide(2,0)
+
+
+# Chaining Decorator
+
+def star(func):
+    def inner(*args, **kwargs):
+        print("*" * 15)
+        func(*args, **kwargs)
+        print("*" * 15)
+    return inner
+
+
+def percent(func):
+    def inner(*args, **kwargs):
+        print("%" * 15)
+        func(*args, **kwargs)
+        print("%" * 15)
+    return inner
+
+
+@star
+@percent
+def printer(msg):
+    print(msg)
+
+printer("Hello")
