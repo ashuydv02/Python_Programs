80fc1b24c8c5d4a6f2edf6c46d00ec07f14a9571
ashuydv02 <ay545153@gmail.com>
2024-06-13 14:52:01 +0530

    Added Super function to access the base class method

--- methodOverriding.py
+++ methodOverriding.py
@@ -1,14 +1,14 @@
 class Animal:
-    name = ''
-
     def eat(self):
         print('I can eat.')
 
 class Dog(Animal):
+    name = ''
+
     def eat(self):
         super().eat()
-        print(f"{self.name} like to eat bones.")
+        print(f"{Dog.name} like to eat bones.")
 
 dog = Dog()
-dog.name = "BullDog"
+Dog.name = "BullDog"
 dog.eat()
